3b3a6ac22f1095be9c9e4fa5caab5aa1e5d89d22
Henriquepb22 <henriquepbdev@gmail.com>
2021-03-02 19:32:33 -0300

    Add one page per shop for better performance

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -4,8 +4,14 @@ import Home from 'pages/Home'
 const Routes = () => {
     return (
         <Switch>
-            <Route path="/shop/:type" exact>
-                <Home />
+            <Route path="/shop/fire" exact>
+                <Home type="fire" />
+            </Route>
+            <Route path="/shop/water" exact>
+                <Home type="water" />
+            </Route>
+            <Route path="/shop/dragon" exact>
+                <Home type="dragon" />
             </Route>
             <Redirect from="*" to="/shop/fire" />
         </Switch>
